cb4efaf4ac41c6aba91f0e7e5b649e05c5d363e7
andryramorasata <andryramorasata@gmail.com>
2024-05-25 12:16:07 -0400

    fix sorting report

--- report.js
+++ report.js
@@ -7,32 +7,20 @@ function printReport(pages){
 }
 
 function sortReport(pages){
-  //using a nested for loop since I never expect this to be used for a page with more than 10k pages
-  let pageLinks = Object.keys(pages);
-  let pageCountArr = Object.values(pages);
-  for (let i = 0; i < pageCountArr.length; i++){
-    for (let j = i + 1; j < pageCountArr.length; j++){
-      if (pageCountArr[i] > pageCountArr[j]){
-        let temp = pageCountArr[j];
-        let tempLink = pageLinks[j];
-        pageCountArr[j] = pageCountArr[i];
-        pageLinks[j] = pageLinks[i];
-        pageCountArr[i] = temp;
-        pageLinks[i] = temp;
-      }
-    }
+  let arrOfPages = [];
+  let result = {}; 
+  let keys = Object.keys(pages);
+  for (let link of keys){
+    console.log(pages[link]);
+    arrOfPages.push({[link]:pages[link]});
   }
-  for (let link of pageLinks){
-    console.log(link);
+  arrOfPages.sort((a,b) => { a - b });
+
+  for (let page of arrOfPages){
+   result[Object.keys(page)[0]] = Object.values(page)[0]
   }
 
-  let results = {};
-
-  for (let i = 0; pages.length; i++){
-   results[pageLinks[i]] = pageCountArr[i]; 
-  }
-  return results 
-
+  return result 
 }
 
 module.exports = {
